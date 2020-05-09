5f51ff0845c54c2563cad18a084c79b3669a6ad9
Nguyen Thanh Binh <ntb9a1@gmail.com>
2020-05-09 14:01:12 +0700

    BeanFactory

--- JwatTraining - BeanFactoryXml/src/main/java/vn/com/cyberlogitec/EducationApp.java
+++ JwatTraining - BeanFactoryXml/src/main/java/vn/com/cyberlogitec/EducationApp.java
@@ -1,10 +1,13 @@
 package vn.com.cyberlogitec;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 import vn.com.cyberlogitec.model.Student;
 import vn.com.cyberlogitec.service.StudentService;
@@ -13,12 +16,18 @@ import vn.com.cyberlogitec.service.StudentServiceImpl;
 public class EducationApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		StudentService studentService = context.getBean("studentService", StudentService.class);
+		ClassPathResource resource = new ClassPathResource("context.xml");
+		DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions(resource);
+		StudentService studentService = factory.getBean("studentService", StudentService.class);
+		
+//		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+//		StudentService studentService = context.getBean("studentService", StudentService.class);
 		
 		studentService.addStudent("Binh","HCM");
 		System.out.println(studentService.getAllStudent().get(0));
-		((ConfigurableApplicationContext) context).close();
+//		((ConfigurableApplicationContext) context).close();
 	}
 	
 }
