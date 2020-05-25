86826012b83e5e741f82e47a2c26ae52e8ee7ee2
huynhtri998 <huynhtri998@gmail.com>
2020-05-25 10:45:59 +0700

    Complete Practice Mybatis Dynamic SQL

--- JwatTraining/src/main/java/vn/com/cyberlogitec/practice_04/MainTest.java
+++ JwatTraining/src/main/java/vn/com/cyberlogitec/practice_04/MainTest.java
@@ -14,48 +14,57 @@ public class MainTest {
 		StudentMapper studentMapper = new StudentMapper();
 
 		// Find All
+
 		/*
 		 * List<Student> student = studentMapper.findAll();
 		 * System.out.println(student.toString());
 		 */
+
 		// Search Student by Name
 
 		/*
-		 * String nameStudent = ""; if (nameStudent.equals("")) { nameStudent = null; }
+		 * String nameStudent = "B"; if (nameStudent.equals("")) { nameStudent = null; }
 		 * List<Student> student = studentMapper.findStudentByName(nameStudent);
 		 * System.out.println(student.toString());
 		 */
 
 		// Search Student by Name Or Phone
+
 		/*
-		 * String nameStudent = ""; String phoneStudent = ""; if
+		 * String nameStudent = ""; String phoneStudent = "90358"; if
 		 * (nameStudent.equals("")) { nameStudent = null; } else if
 		 * (phoneStudent.equals("")) { phoneStudent = null; } List<Student> student =
 		 * studentMapper.findStudentByNameOrPhone(nameStudent, phoneStudent);
-		 */
-
-		// Search Student by Multi Id
-		/*
-		 * List<Integer> listId = new ArrayList<Integer>(); List<Student> student =
-		 * studentMapper.findStudentByMultiId(listId);
 		 * System.out.println(student.toString());
+		 */
+		// Search Student by Multi Id
+
+		/*
+		 * List<Integer> listId = new ArrayList<Integer>(); listId.add(7);
+		 * listId.add(8); listId.add(9); List<Student> student =
+		 * studentMapper.findStudentByMultiId(listId); if (student.isEmpty()) {
+		 * System.out.println("Không có KQ"); } else {
+		 * System.out.println(student.toString()); }
 		 */
 
 		// Update Student by ID
 
 		/*
-		 * int ida = 0; Student student = studentMapper.findStudentById(ida); if
+		 * int ida = 7; Student student = studentMapper.findStudentById(ida); if
 		 * (student != null) { student.setEMAIL("abc@adwwad");
 		 * student.setBRANCH("zxcv"); String result =
-		 * studentMapper.updateStudent(student); System.out.println(result); }else {
+		 * studentMapper.updateStudent(student); System.out.println(result);
+		 * System.out.println(student.toString()); } else {
 		 * System.out.println("Thất bại"); }
 		 */
 
 		// Insert Student
 
-		Student student = new Student("Huynh Minh Tri", "bcd", 100, "0359941234", "abc@abc");
-		String result = studentMapper.saveStudent(student);
-		System.out.println(result);
+		/*
+		 * Student student = new Student("Huynh Minh Tri", "bcd", 100, "0359941234",
+		 * "abc@abc"); String result = studentMapper.saveStudent(student);
+		 * System.out.println(result);
+		 */
 
 	}
 
